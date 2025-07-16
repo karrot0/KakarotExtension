1365849eee1adaab3404fbc5640cca50f834f538
karrot0 <karrotgaming1@gmail.com>
2025-07-16 17:28:02 -0400

    fix: Standardize string quotes in CaveInterceptor and bump version to 1.0.0-alpha.5

--- src/Batcave/BatcaveInterceptor.ts
+++ src/Batcave/BatcaveInterceptor.ts
@@ -3,10 +3,10 @@ import { PaperbackInterceptor, Request, Response } from "@paperback/types";
 export class CaveInterceptor extends PaperbackInterceptor {
   override async interceptRequest(request: Request): Promise<Request> {
     let referer = `https://batcave.biz`;
-    if (request.url.includes("readcomiconline.ru")) {
+    if (request.url.includes('readcomiconline.ru')) {
       referer = `https://readcomiconline.ru`;
     }
-    
+
     request.headers = {
       ...request.headers,
       origin: referer,
