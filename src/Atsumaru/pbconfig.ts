db45000f6976d6872cbf947bf0e3c0e5a8ac6482
karrot0 <karrotgaming1@gmail.com>
2025-10-11 17:57:07 -0400

    Lucifers Circl Atsumaru fix

--- src/Atsumaru/pbconfig.ts
+++ src/Atsumaru/pbconfig.ts
@@ -1,21 +1,24 @@
 import { ContentRating, SourceInfo, SourceIntents } from "@paperback/types";
 
 export default {
-  name: "Atsumaru",
-  description: "Extension that pulls content from atsu.moe",
-  version: "1.0.0-alpha.1",
-  icon: "icon.png",
-  language: "English",
-  contentRating: ContentRating.EVERYONE,
-  capabilities: [
-    SourceIntents.DISCOVER_SECIONS,
-    SourceIntents.MANGA_SEARCH,
-    SourceIntents.MANGA_CHAPTERS,
-  ],
-  badges: [],
-  developers: [
-    {
-      name: "Karrot",
-    }
-  ],
+    name: "Atsumaru",
+    description: "Extension that pulls content from atsu.moe",
+    version: "1.0.0-alpha.1",
+    icon: "icon.png",
+    language: "English",
+    contentRating: ContentRating.EVERYONE,
+    capabilities: [
+        SourceIntents.DISCOVER_SECIONS,
+        SourceIntents.MANGA_SEARCH,
+        SourceIntents.MANGA_CHAPTERS,
+    ],
+    badges: [],
+    developers: [
+        {
+            name: "Karrot",
+        },
+        {
+            name: "LucifersCircle"
+        }
+    ],
 } satisfies SourceInfo;
