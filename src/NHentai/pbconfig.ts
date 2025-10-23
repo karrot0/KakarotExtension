53d0b5411b9b3b727f3dbf73b8abfae1ea41ba3e
pisshammy <bamhamyt1@gmail.com>
2025-10-23 12:09:00 -0500

    AI slop. Added search filters for the number of pages and favorites. Added sorting options for search. Added an include/exclude filter option for popular tags. Added a feature to track read manga, and an option to hide those manga. Fixed the occasional blank thumbnail issue. Added argument option in settings. Subtitles have useful information following the format of "rd (if chap has been read/clicked) | EN | 122 (for # of pages) | 25000 (for # of faves)." Updated verison to 2.0.

--- src/NHentai/pbconfig.ts
+++ src/NHentai/pbconfig.ts
@@ -3,7 +3,7 @@ import { ContentRating, SourceInfo, SourceIntents } from "@paperback/types";
 export default {
   name: "NHentai",
   description: "Extension that pulls content from nhentai.net",
-  version: "1.0.0-alpha.1",
+  version: "2.0.0",
   icon: "icon.png",
   language: "multi",
   contentRating: ContentRating.ADULT,
