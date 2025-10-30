5419e7bd5c75403ee7d1c7532dec1ffc9d5f3d33
karrot0 <karrotgaming1@gmail.com>
2025-10-29 21:35:41 -0400

    Bump version to 1.0.0-alpha.2 in pbconfig.ts

--- src/Elftoon/pbconfig.ts
+++ src/Elftoon/pbconfig.ts
@@ -3,7 +3,7 @@ import { ContentRating, SourceInfo, SourceIntents } from "@paperback/types";
 export default {
   name: "Elftoon",
   description: "Extension that pulls content from elftoon.com (Cloudflare protected).",
-  version: "1.0.0-alpha.1",
+  version: "1.0.0-alpha.2",
   icon: "icon.png",
   language: "en",
   contentRating: ContentRating.EVERYONE,
