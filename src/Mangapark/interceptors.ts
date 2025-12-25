754fe9dffeabc719ee696a9320d1a7a885469415
karrot0 <karrotgaming1@gmail.com>
2025-12-24 22:40:22 -0500

    fix(interceptors): update referer and origin headers to use dynamic URL fix(main): modify URL regex to allow any domain for media links

--- src/Mangapark/interceptors.ts
+++ src/Mangapark/interceptors.ts
@@ -2,9 +2,11 @@ import { PaperbackInterceptor, Request, Response } from "@paperback/types";
 
 export class Interceptor extends PaperbackInterceptor {
   override async interceptRequest(request: Request): Promise<Request> {
+    const url = new URL(request.url);
     request.headers = {
       ...request.headers,
-      referer: `https://mangapark.io/`,
+      referer: `${url.protocol}//${url.host}/`,
+      origin: `${url.protocol}//${url.host}`,
       "user-agent": await Application.getDefaultUserAgent(),
       cookie: "nsfw=2",
     };
