59193e3af2a5df04a10e3c9fb6b8c367f9dad307
karrot0 <karrotgaming1@gmail.com>
2025-12-24 23:03:44 -0500

    fix(interceptors): revert to static referer and origin headers

--- src/Mangapark/interceptors.ts
+++ src/Mangapark/interceptors.ts
@@ -2,12 +2,9 @@ import { PaperbackInterceptor, Request, Response } from "@paperback/types";
 
 export class Interceptor extends PaperbackInterceptor {
   override async interceptRequest(request: Request): Promise<Request> {
-    const match = request.url.match(/^(https?:\/\/[^\/]+)/);
-    const origin = match ? match[1] : "https://mangapark.io";
     request.headers = {
       ...request.headers,
-      referer: `${origin}/`,
-      origin: origin,
+      referer: `https://mangapark.io/`,
       "user-agent": await Application.getDefaultUserAgent(),
       cookie: "nsfw=2",
     };
