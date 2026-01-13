a81f4b439f1d36159e220329b0116fcc2ba8b0a0
karrot0 <karrotgaming1@gmail.com>
2026-01-13 09:17:46 -0500

    fix(pbconfig): update version from 1.2 to 1.3

--- src/Batcave/pbconfig.ts
+++ src/Batcave/pbconfig.ts
@@ -3,7 +3,7 @@ import { ContentRating, SourceInfo, SourceIntents } from "@paperback/types";
 export default {
   name: "Batcave",
   description: "Extension that pulls content from batcave.biz.",
-  version: "1.2",
+  version: "1.3",
   icon: "icon.png",
   language: "en",
   contentRating: ContentRating.EVERYONE,
