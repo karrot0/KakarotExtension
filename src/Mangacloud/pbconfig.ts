6a7e3f0b04f985db4d663ec7481dc3bec64c7cac
karrot0 <karrotgaming1@gmail.com>
2026-04-28 13:26:13 -0400

    chore(Mangacloud): bump version

--- src/Mangacloud/pbconfig.ts
+++ src/Mangacloud/pbconfig.ts
@@ -3,7 +3,7 @@ import { ContentRating, ExtensionInfo, SourceIntents } from "@paperback/types";
 export default {
   name: "Mangacloud",
   description: "Extension that pulls content from https://mangacloud.org/",
-  version: "1.0.0-alpha.1",
+  version: "1.0.0-alpha.2",
   icon: "icon.png",
   language: "en",
   contentRating: ContentRating.EVERYONE,
